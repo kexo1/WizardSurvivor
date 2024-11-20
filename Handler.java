712db79e4e9e1d1c1ee76294da5d86ee4654c34e
Kexo <martinpolansky@proton.me>
2024-11-21 00:41:33 +0100

    Added UML

--- Handler.java
+++ Handler.java
@@ -39,28 +39,28 @@ public class Handler {
         return up;
     }
 
-    public void setUp(boolean up) {
-        this.up = up;
-    }
-
     public boolean isDown() {
         return down;
-    }
-
-    public void setDown(boolean down) {
-        this.down = down;
     }
 
     public boolean isLeft() {
         return left;
     }
 
-    public void setLeft(boolean left) {
-        this.left = left;
-    }
-
     public boolean isRight() {
         return right;
+    }
+
+    public void setUp(boolean up) {
+        this.up = up;
+    }
+
+    public void setDown(boolean down) {
+        this.down = down;
+    }
+
+    public void setLeft(boolean left) {
+        this.left = left;
     }
 
     public void setRight(boolean right) {
